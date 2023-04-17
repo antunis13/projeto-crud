e00123d0dc6c54ee2c84f86c8584bdeb0d3dbef8
Richard Antunis <richardantunis4@gmail.com>
2023-04-17 19:41:32 -0300

    adding initial folder structure

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Testando 1 2 3 '
+    })
+})
+
+app.use((req, res)=>{
+    res.send('Pagina não encontrada')
+})
+
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`Server is listening on port ${port}`) )
